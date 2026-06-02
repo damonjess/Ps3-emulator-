a397acd1e1545a5b9ecde0b924b845ab320bf083
damonjess <damonkirby86@gmail.com>
2026-06-02 11:41:02 +0100

    update

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ps3-emulator"
+include(":app")
+project(":app").projectDir = file("RetroRTS/app")
